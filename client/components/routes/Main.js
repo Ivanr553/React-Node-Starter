51da61e065789fa5f933681c27fa214a8fdbac90
ivanr553 <ivanr553@gmail.com>
2017-10-16 00:43:12 -0700

    cleaning up client code

--- client/components/routes/Main.js
+++ client/components/routes/Main.js
@@ -9,10 +9,10 @@ class Main extends Component {
       <div className="Main">
         <div className="header container" > Welcome to your React - Node starter! </div>
         <div className="container">
-          <div className="React" >
+          <div>
             <img className="react-png" src={reactPng} width="275px" height="200px" />
           </div>
-          <div className="Node" >
+          <div>
             <img className="node-png" src={nodePng} width="275px" height="275px" />
           </div>
         </div>
